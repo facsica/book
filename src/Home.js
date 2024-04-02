cd5c322a7f3eabb72c9611e469d21188f1967468
pooh <6442086926@student.chula.ac.th>
2024-04-03 03:00:55 +0700

    add image in home page and fading effect

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home_container'>
+         {/*image show is 1800x300 and fading for rest of it */}
+        <img src='https://stmarksdp.org/app/uploads/Banner-1800x600-image.jpg'></img>  
+      </div>
+    </div>
+  )
+}
+
+export default Home
