b991c0d84e189557c22824085df0140dbce59c85
pooh <6442086926@student.chula.ac.th>
2024-04-03 01:58:06 +0700

    can add icon from mui

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAhOD4HbED1PisoOhy2LJNowgHYhZpRITw",
+    authDomain: "e-book-store-db.firebaseapp.com",
+    projectId: "e-book-store-db",
+    storageBucket: "e-book-store-db.appspot.com",
+    messagingSenderId: "225931014364",
+    appId: "1:225931014364:web:d39990c91aaf1cbe188419",
+    measurementId: "G-3SL6BFCQ2E"
+  };
