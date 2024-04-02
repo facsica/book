76982a213139039830adaa85ef948bc96c981372
pooh <6442086926@student.chula.ac.th>
2024-04-03 04:37:59 +0700

    don't know how to align rating

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import "./Home.css";
+import Product from './Product';
 
 function Home() {
   return (
     <div className='home'>
       <div className='home_container'>
          {/*image show is 1800x300 and fading for rest of it */}
-        <img src='https://stmarksdp.org/app/uploads/Banner-1800x600-image.jpg'></img>  
+        <img className='home_image' src='https://stmarksdp.org/app/uploads/Banner-1800x600-image.jpg'></img>  
+
+        <div className='home_row'>
+            <Product title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses" price= "29.99" image='https://m.media-amazon.com/images/I/61BFOf9Ap-L._AC_UF1000,1000_QL80_.jpg' rating={5}/>
+            <Product title='The lean start up ' price= "29.99" image='https://m.media-amazon.com/images/I/61BFOf9Ap-L._AC_UF1000,1000_QL80_.jpg' rating={5}/>
+        </div>
+
+        <div className='home_row'>
+            <Product title='The lean start up ' price= "29.99" image='https://m.media-amazon.com/images/I/61BFOf9Ap-L._AC_UF1000,1000_QL80_.jpg' rating={5}/>
+            <Product title='The lean start up ' price= "29.99" image='https://m.media-amazon.com/images/I/61BFOf9Ap-L._AC_UF1000,1000_QL80_.jpg' rating={5}/>
+            <Product title='The lean start up ' price= "29.99" image='https://m.media-amazon.com/images/I/61BFOf9Ap-L._AC_UF1000,1000_QL80_.jpg' rating={5}/>
+        </div>
+
+        <div className='home_row'>
+            <Product title='The lean start up ' price= "29.99" image='https://m.media-amazon.com/images/I/61BFOf9Ap-L._AC_UF1000,1000_QL80_.jpg' rating={5}/>
+        </div>
+
       </div>
     </div>
   )
